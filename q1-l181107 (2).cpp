bfacdea67688af81dfde7319c4ca30de03d05b8a
l181107abdullahkhan <46881904+l181107abdullahkhan@users.noreply.github.com>
2019-01-21 10:44:10 +0500

    Update q1-l181107 (2).cpp

--- q1-l181107 (2).cpp
+++ q1-l181107 (2).cpp
@@ -1,2 +1,34 @@
 
 
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	int num = 0;
+	cin >> num;
+	for (int i = 1; i <= num; i++)
+	{
+		for (int j = 0; j < i; j++)
+			cout << i;
+		for (int j = 0; j < 2*(num-i); j++)
+			cout << "*";
+		for (int j = 0; j < i; j++)
+			cout << i;
+
+		cout << endl;
+	}
+	for (int i = 1; i < num; i++)
+	{
+		for (int j = 0; j < num-i; j++)
+			cout << i + num;
+		for (int j = 0; j < 2*i; j++)
+			cout << "*";
+		for (int j = 0; j < num - i; j++)
+			cout << i + num;
+
+		cout << endl;
+	}
+
+
+}
